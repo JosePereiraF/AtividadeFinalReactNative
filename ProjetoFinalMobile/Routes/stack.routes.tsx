78c478724b962fce89b39529c9b254da4498b3be
Kalli-E <kcruzcarmo@gmail.com>
2024-06-28 18:17:40 -0300

    correção das rotas

--- ProjetoFinalMobile/Routes/stack.routes.tsx
+++ ProjetoFinalMobile/Routes/stack.routes.tsx
@@ -13,10 +13,10 @@ const Stack = createNativeStackNavigator();
 export default function HomeStack() {
    return (
     <Stack.Navigator screenOptions={{title: ''}}>
-     <Stack.Screen name="Home" component={Inicio} />
-     <Stack.Screen name="Avaliação" component={Avaliacao} />
-      <Stack.Screen name="Treino Masculino" component={Training} />
-      <Stack.Screen name="Treino Feminino" component={TreinoFeminino} />
+     <Stack.Screen name="Inicio" component={Inicio} />
+     <Stack.Screen name="Avaliacao" component={Avaliacao} />
+      <Stack.Screen name="Training" component={Training} />
+      <Stack.Screen name="TreinoFeminino" component={TreinoFeminino} />
       <Stack.Screen name="Cadastro" component={Cadastro} />
     </Stack.Navigator>
    );
